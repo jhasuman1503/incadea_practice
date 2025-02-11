88e7e884b10a3bfd0b53a03cdf8213f30e2162e1
Suman Jha <suman.jha@incadea.com>
2025-02-11 14:40:48 +0530

    inits

--- practise1/obj/Debug/net8.0/practise1.AssemblyInfo.cs
+++ practise1/obj/Debug/net8.0/practise1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practise1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ee8fc2b81e93f46d7d94b69be042ecedc9513e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("practise1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practise1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
